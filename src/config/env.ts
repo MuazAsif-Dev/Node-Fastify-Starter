e4af3911d3bfca7adbfcc1e2fee4a7eb43e8d81b
MuazAsif-Dev <muazasif.info@gmail.com>
2023-10-30 05:07:39 -0400

    feat: add more specific typings in the envSchema

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-	PORT: z.number().positive().default(8000),
-	NODE_ENV: z.enum(["development", "production"]).default("development"),
-	DATABASE_URI: z.string(),
+	PORT: z.number().int().positive().default(8000),
+	NODE_ENV: z
+		.enum(["development", "staging", "production"])
+		.default("development"),
+	DATABASE_URI: z.string().url().startsWith("postgres://"),
 });
 
 export const env = envSchema.parse(process.env);
