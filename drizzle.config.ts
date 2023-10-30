3f01b61ada1d7f8a7f82fe9c66ba7f55387244fe
MuazAsif-Dev <muazasif.info@gmail.com>
2023-10-30 15:02:42 -0400

    feat: add additional fields in drizzle config for drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "drizzle-kit";
+import { env } from "./src/config/env";
 
 export default {
 	schema: "./src/db/schemas/*",
 	out: "./src/db/migrations",
+	driver: "pg",
+	dbCredentials: {
+		connectionString: env.DATABASE_URI,
+	},
 	breakpoints: false,
 } satisfies Config;
