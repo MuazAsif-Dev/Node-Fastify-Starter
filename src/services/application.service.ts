de81c7ee23d0495c65e198b23d41580adeb31563
MuazAsif-Dev <muazasif.info@gmail.com>
2023-10-30 16:12:39 -0400

    feat: add application service

---
+++ src/services/application.service.ts
@@ -0,0 +1,24 @@
+import { InferInsertModel } from "drizzle-orm";
+
+import { db } from "@/db/dbConnection";
+import { applications } from "@/db/schemas/application.schema";
+
+export async function createApplication(
+	data: InferInsertModel<typeof applications>,
+) {
+	const result = await db.insert(applications).values(data).returning();
+
+	return result[0];
+}
+
+export async function getApplication() {
+	const result = await db
+		.select({
+			id: applications.id,
+			name: applications.name,
+			createdAt: applications.createdAt,
+		})
+		.from(applications);
+
+	return result[0];
+}
