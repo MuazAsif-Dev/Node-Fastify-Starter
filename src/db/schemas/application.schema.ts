d79950ae0572f166da4f9d55911988ddfaef86b5
MuazAsif-Dev <muazasif.info@gmail.com>
2023-10-30 16:12:11 -0400

    feat: add application request type along with zod and json schemas

--- src/db/schemas/application.schema.ts
+++ src/db/schemas/application.schema.ts
@@ -1,4 +1,7 @@
 import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
+import zodToJsonSchema from "zod-to-json-schema";
 
 export const applications = pgTable("applications", {
 	id: uuid("id").primaryKey().defaultRandom(),
@@ -6,3 +9,18 @@ export const applications = pgTable("applications", {
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 	updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+const createApplicationApiValidatorSchema = createInsertSchema(
+	applications,
+).pick({ name: true });
+
+export type createApplicationRequestType = z.infer<
+	typeof createApplicationApiValidatorSchema
+>;
+
+export const createApplicationJsonSchema = {
+	body: zodToJsonSchema(
+		createApplicationApiValidatorSchema,
+		"createApplicationApiValidatorSchema",
+	),
+};
