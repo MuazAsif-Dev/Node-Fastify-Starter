36cff8dcede98b6f119af2b3dea559a91ede9ab8
MuazAsif-Dev <muazasif.info@gmail.com>
2023-11-03 09:18:56 -0400

    feat: add argon2 for password hashing

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,17 @@
+import * as argon2 from "argon2";
 import { InferInsertModel } from "drizzle-orm";
 
 import { db } from "@/db/dbConnection";
 import { users } from "@/db/models/users.model";
 
 export async function createUser(data: InferInsertModel<typeof users>) {
+	const hashedPassword = await argon2.hash(data.password);
+
 	const result = await db
 		.insert(users)
 		.values({
 			...data,
+			password: hashedPassword,
 		})
 		.returning({
 			id: users.id,
