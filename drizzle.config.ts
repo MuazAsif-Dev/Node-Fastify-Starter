ac986e872240d2f123c16c6b8ab26b4d23c86619
MuazAsif-Dev <muazasif.info@gmail.com>
2023-11-04 06:46:39 -0400

    chore: update schema path of drizzle config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 import { env } from "./src/config/env";
 
 export default {
-	schema: "./src/db/schemas/*",
+	schema: "./src/db/models/*",
 	out: "./src/db/migrations",
 	driver: "pg",
 	dbCredentials: {
