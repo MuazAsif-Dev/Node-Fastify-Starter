892dd2854514ad52a5cab990eeb27f0f706acf3c
MuazAsif-Dev <muazasif.info@gmail.com>
2023-11-04 07:17:37 -0400

    chore: remove auth module and routes

--- src/modules/v1/routes.ts
+++ src/modules/v1/routes.ts
@@ -1,7 +1,6 @@
 import type { FastifyInstance } from "fastify";
 
 import applicationRouter from "./applications/applications.route";
-import authRouter from "./auth/auth.route";
 import userRouter from "./users/users.route";
 
 export default async function router(router: FastifyInstance) {
@@ -9,7 +8,6 @@ export default async function router(router: FastifyInstance) {
 		return { message: "Api v1 is running" };
 	});
 
-	router.register(authRouter, { prefix: "/auth" });
 	router.register(applicationRouter, { prefix: "/application" });
 	router.register(userRouter, { prefix: "/user" });
 }
