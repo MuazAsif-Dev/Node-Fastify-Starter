4766b1996ca42f4a745c5acd7418ef973fb5bf4b
MuazAsif-Dev <muazasif.info@gmail.com>
2023-11-10 02:33:29 -0500

    feat: add user service methods

--- src/modules/v1/users/users.service.ts
+++ src/modules/v1/users/users.service.ts
@@ -1,7 +1,8 @@
 import * as argon2 from "argon2";
-import { eq, InferInsertModel } from "drizzle-orm";
+import { and, eq, InferInsertModel } from "drizzle-orm";
 
 import { db } from "@/db/dbConnection";
+import { roles } from "@/db/models/roles.model";
 import { users } from "@/db/models/users.model";
 import { usersToRoles } from "@/db/models/usersToRoles.model";
 
@@ -40,4 +41,66 @@ export async function assignRoleToUser(
 	const result = await db.insert(usersToRoles).values(data).returning();
 
 	return result[0];
+}
+
+export async function getUserByEmail({
+	email,
+	applicationId,
+}: {
+	email: string;
+	applicationId: string;
+}) {
+	const result = await db
+		.select({
+			id: users.id,
+			email: users.email,
+			name: users.name,
+			applicationId: users.applicationId,
+			roleId: roles.id,
+			password: users.password,
+			permissions: roles.permissions,
+		})
+		.from(users)
+		.where(and(eq(users.email, email), eq(users.applicationId, applicationId)))
+		.leftJoin(
+			usersToRoles,
+			and(
+				eq(usersToRoles.userId, users.id),
+				eq(usersToRoles.applicationId, users.applicationId),
+			),
+		)
+		.leftJoin(roles, eq(roles.id, usersToRoles.roleId));
+
+	if (!result.length) {
+		return null;
+	}
+
+	const user = result.reduce(
+		(acc, curr) => {
+			if (!acc.id) {
+				return {
+					...curr,
+					permissions: new Set(curr.permissions),
+				};
+			}
+
+			if (!curr.permissions) {
+				return acc;
+			}
+
+			for (const permission of curr.permissions) {
+				acc.permissions.add(permission);
+			}
+
+			return acc;
+		},
+		{} as Omit<(typeof result)[number], "permissions"> & {
+			permissions: Set<string>;
+		},
+	);
+
+	return {
+		...user,
+		permissions: Array.from(user.permissions),
+	};
 }
