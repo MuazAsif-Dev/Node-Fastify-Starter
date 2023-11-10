4e1c8bf4bcd1c5d9caf875996399afd206098f37
MuazAsif-Dev <muazasif.info@gmail.com>
2023-11-10 02:34:54 -0500

    feat: update user module routes

--- src/modules/v1/users/users.route.ts
+++ src/modules/v1/users/users.route.ts
@@ -1,10 +1,26 @@
 import type { FastifyInstance } from "fastify";
 
-import { createUserHandler } from "./users.controller";
-import { createUserJsonSchema } from "./users.schema";
+import {
+	assignRoleTouserHandler,
+	createUserHandler,
+	loginHandler,
+} from "./users.controller";
+import {
+	createUserJsonSchema,
+	createUserToRolesJsonSchema,
+	loginUserJsonSchema,
+} from "./users.schema";
 
 export default async function userRouter(router: FastifyInstance) {
 	router.post("/", { schema: createUserJsonSchema }, createUserHandler);
+
+	router.post("/login", { schema: loginUserJsonSchema }, loginHandler);
+
+	router.post(
+		"/roles",
+		{ schema: createUserToRolesJsonSchema },
+		assignRoleTouserHandler,
+	);
 
 	router.get("/", () => {
 		return 1;
