001664666d8db9a870f0173cc4b0addb611c6003
MuazAsif-Dev <muazasif.info@gmail.com>
2023-11-10 02:34:34 -0500

    feat: update user module schema

--- src/modules/v1/users/users.schema.ts
+++ src/modules/v1/users/users.schema.ts
@@ -1,8 +1,9 @@
-import { createInsertSchema } from "drizzle-zod";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 import zodToJsonSchema from "zod-to-json-schema";
 
 import { users } from "@/db/models/users.model";
+import { usersToRoles } from "@/db/models/usersToRoles.model";
 
 const createUserTableSchema = createInsertSchema(users, {
 	email: (schema) => schema.email.email(),
@@ -22,5 +23,45 @@ export const createUserJsonSchema = {
 	body: zodToJsonSchema(
 		createUserApiValidatorSchema,
 		"createUserApiValidatorSchema",
+	),
+};
+
+const selectUserTableSchema = createSelectSchema(users, {
+	email: (schema) => schema.email.email(),
+	applicationId: (schema) => schema.applicationId.uuid(),
+	password: (schema) => schema.password.min(6),
+});
+
+const loginUserApiValidatorSchema = selectUserTableSchema.pick({
+	applicationId: true,
+	email: true,
+	password: true,
+});
+
+export type loginUserRequestBodyType = z.infer<
+	typeof loginUserApiValidatorSchema
+>;
+
+export const loginUserJsonSchema = {
+	body: zodToJsonSchema(
+		loginUserApiValidatorSchema,
+		"loginUserApiValidatorSchema",
+	),
+};
+
+const createUserToRolesTableSchema = createInsertSchema(usersToRoles, {
+	userId: z.string().uuid(),
+	applicationId: z.string().uuid(),
+	roleId: z.string().uuid(),
+});
+
+export type createUserToRolesRequestBodyType = z.infer<
+	typeof createUserToRolesTableSchema
+>;
+
+export const createUserToRolesJsonSchema = {
+	body: zodToJsonSchema(
+		createUserToRolesTableSchema,
+		"createUserToRolesTableSchema",
 	),
 };
